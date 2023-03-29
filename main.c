70135f89bbbeea491f4291b8443ea1ab9538864c
kvasa <kvasan2104@gmail.com>
2023-03-29 10:24:55 +0300

    Correct sorting algorithm

--- main.c
+++ main.c
@@ -3,39 +3,43 @@
 #include <string.h>
 
 
-void swap(char *a, char *b) {
-    char temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(char *first_char, char *second_char) {
+    char temporary = *first_char;
+    *first_char = *second_char;
+    *second_char = temporary;
 }
 
-int partition(char arr[], int low, int high) {
-    char pivot = arr[high];
+int partition(char array[], int low, int high) {
+    char pivot = array[high];
     int i = (low - 1);
     for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
+        if (array[j] < pivot) {
             i++;
-            swap(&arr[i], &arr[j]);
+            swap(&array[i], &array[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
+    swap(&array[i + 1], &array[high]);
     return (i + 1);
 }
 
-void quickSort(char arr[], int low, int high) {
+void quickSort(char array[], int low, int high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pivot_index = partition(array, low, high);
+        quickSort(array, low, pivot_index - 1);
+        quickSort(array, pivot_index + 1, high);
     }
 }
 
-int main() {
-    char arr[] = {'a', 'd', 'b', 'c', 'f', 'e'};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, n - 1);
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++)
-        printf("%c ", arr[i]);
-    return 0;
+int main(int argc, char **argv) {
+    if (argc == 4) {
+        int array_size = strtol(argv[1], NULL, 10);
+        char array[array_size];
+        char character[2];
+        strcpy(array, argv[2]);
+        strcpy(character, argv[3]);
+        quickSort(array, 0, array_size - 1);
+        for (int i = 0; i < array_size; i++)
+            printf("%c ", array[i]);
+    } else
+        printf("wrong number of arguments");
 }
